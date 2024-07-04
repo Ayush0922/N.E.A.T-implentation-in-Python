fb9f39a24046fc166b126681ad278a97fb0ace9b
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-07-04 14:15:48 +0530

    Update game.py
    created base class

--- game.py
+++ game.py
@@ -127,5 +127,23 @@ class Pipe:
            return True
         return False
 
-
+class Base:
+    VEL = 3.5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+    def __init__(self,y): 
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        
+        if self.x1 + self.WIDTH <0:
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+           self.x2 = self.x1 + self.WIDTH
+    def draw(self,win):
+        win.blit(self.IMG,(self.x1,self.y))
+        win.blit(self.IMG,(self.x2,self.y))
 
