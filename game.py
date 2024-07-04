1974dafd64aa860492c6ad9628a9eab361991444
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-07-04 14:09:11 +0530

    Update game.py
    initialized image constants and font for score

--- game.py
+++ game.py
@@ -4,3 +4,16 @@ import os
 import time
 import random
 pygame.font.init()
+
+WIND_WIDTH = 500
+WIND_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
+
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
+
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans",50)
